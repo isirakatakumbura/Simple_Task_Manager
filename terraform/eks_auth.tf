e1952b1b42126a3d4fd580ee0daf2f1a4defa0e1
Isira Katakumbura <isirakatakumburs@gmail.com>
2026-02-11 18:55:42 -0700

    Re-arrange terrafomr config to resolve issues with EKS authentication

--- terraform/eks_auth.tf
+++ terraform/eks_auth.tf
@@ -2,8 +2,6 @@ module "eks_auth" {
   source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
   version = "~> 20.0"
 
-  manage_aws_auth = true
-
   aws_auth_users = [
     {
       userarn  = "arn:aws:iam::463470938828:user/Isira"
